163d7129126fd1c4c89b41fff9d2f353300973e2
iamvishal345 <vishalagnihotri345@gmail.com>
2020-09-12 22:41:27 +0530

    Updates

---
+++ precache-manifest.4e1d1e3ea6e81aa60d0ee1609742dd71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50304761d8aa23a9f9bc85039aa73494",
+    "url": "/random-quote-generator/index.html"
+  },
+  {
+    "revision": "a3534c9fff56f3109a16",
+    "url": "/random-quote-generator/static/css/main.19764903.chunk.css"
+  },
+  {
+    "revision": "525e4e8c6589afee802d",
+    "url": "/random-quote-generator/static/js/2.e930acd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-generator/static/js/2.e930acd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3534c9fff56f3109a16",
+    "url": "/random-quote-generator/static/js/main.9d4f579f.chunk.js"
+  },
+  {
+    "revision": "efbd8013faa4b47d47b4",
+    "url": "/random-quote-generator/static/js/runtime-main.04b377e6.js"
+  },
+  {
+    "revision": "f7e262503c546f8f5b665832bd7bba01",
+    "url": "/random-quote-generator/static/media/twitter.f7e26250.svg"
+  }
+]);
